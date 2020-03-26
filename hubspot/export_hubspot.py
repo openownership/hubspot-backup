a286eeeb8ac738f38dc85c39687b8d75fd2f62fd
Steve Day <steve@openownership.org>
2020-03-26 15:41:49 +0000

    Backup tickets and contact country/persona

--- hubspot/export_hubspot.py
+++ hubspot/export_hubspot.py
@@ -8,29 +8,20 @@ from hubspot3 import Hubspot3
 load_dotenv()
 
 
-def downloadContacts(client):
-    contacts = client.contacts.get_all()
-    with open('backup/contacts.json', 'w') as f:
-        f.write(json.dumps(contacts, indent=2, sort_keys=True))
-
-
-def downloadCompanies(client):
-    companies = client.companies.get_all()
-    with open('backup/companies.json', 'w') as f:
-        f.write(json.dumps(companies, indent=2, sort_keys=True))
-
-
-def downloadEngagements(client):
-    engagements = client.engagements.get_all()
-    with open('backup/engagements.json', 'w') as f:
-        f.write(json.dumps(engagements, indent=2, sort_keys=True))
+def download(data, name):
+    with open('backup/{}.json'.format(name), 'w') as f:
+        f.write(json.dumps(data, indent=2, sort_keys=True))
 
 
 def main():
     client = Hubspot3(api_key=os.getenv('HUBSPOT_API_KEY'))
-    downloadContacts(client)
-    downloadCompanies(client)
-    downloadEngagements(client)
+    download(
+        client.contacts.get_all(extra_properties=['hs_persona', 'country']),
+        'contacts'
+    )
+    download(client.companies.get_all(), 'companies')
+    download(client.engagements.get_all(), 'engagements')
+    download(client.tickets.get_all(), 'tickets')
 
 
 if __name__ == "__main__":
